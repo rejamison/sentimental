23cff2adf891ff8c0601848ba7d4c76be6f98a66
Robert Jamison <rejamison@gmail.com>
2017-06-18 22:38:55 -0700

    Updated CAD files to seperate base from interior and fine-tune tolerances of holes.

--- viz.py
+++ viz.py
@@ -1,14 +1,55 @@
 from vpython import *
+from random import *
 
 gorigin = vector(0, 0, 0)
 gaxis = vector(0, 1, 0)
-g[0] = sphere(pos=vector(0, 0, 1), radius=0.1, color=color.red)
-g[1] = sphere(pos=vector(0, 0, -1), radius=0.1, color=color.red)
-g[2] = sphere(pos=vector(0, 1, 0), radius=0.1, color=color.red)
-g[3] = sphere(pos=vector(1, 0, 0), radius=0.1, color=color.red)
-g[4] = sphere(pos=vector(-1, 0, 0), radius=0.1, color=color.red)
+sphere_size = 0.1
+default_color = vector(1.0, 0, 0)
+g = []
+g.append(sphere(pos=vector(0, 1, 0), radius=sphere_size, color=default_color))  # top
+
+g.append(sphere(pos=vector(0, 0, 1), radius=sphere_size, color=default_color))
+g.append(sphere(pos=vector(0, 0, -1), radius=sphere_size, color=default_color))
+g.append(sphere(pos=vector(sin(pi/4), 0, cos(pi/4)), radius=sphere_size, color=default_color))
+g.append(sphere(pos=vector(sin(pi/4), 0, -cos(pi/4)), radius=sphere_size, color=default_color))
+g.append(sphere(pos=vector(1, 0, 0), radius=sphere_size, color=default_color))
+g.append(sphere(pos=vector(-sin(pi/4), 0, cos(pi/4)), radius=sphere_size, color=default_color))
+g.append(sphere(pos=vector(-sin(pi/4), 0, -cos(pi/4)), radius=sphere_size, color=default_color))
+g.append(sphere(pos=vector(-1, 0, 0), radius=sphere_size, color=default_color))
+
+g.append(sphere(pos=vector(0, cos(pi/4), sin(pi/4)), radius=sphere_size, color=default_color))
+g.append(sphere(pos=vector(0, cos(pi/4), -sin(pi/4)), radius=sphere_size, color=default_color))
+g.append(sphere(pos=vector(sin(pi/4) * cos(pi/4), cos(pi/4), cos(pi/4) * cos(pi/4)), radius=sphere_size, color=default_color))
+g.append(sphere(pos=vector(sin(pi/4) * cos(pi/4), cos(pi/4), -cos(pi/4) * cos(pi/4)), radius=sphere_size, color=default_color))
+g.append(sphere(pos=vector(cos(pi/4), cos(pi/4), 0), radius=sphere_size, color=default_color))
+g.append(sphere(pos=vector(-sin(pi/4) * cos(pi/4), cos(pi/4), cos(pi/4) * cos(pi/4)), radius=sphere_size, color=default_color))
+g.append(sphere(pos=vector(-sin(pi/4) * cos(pi/4), cos(pi/4), -cos(pi/4) * cos(pi/4)), radius=sphere_size, color=default_color))
+g.append(sphere(pos=vector(-cos(pi/4), cos(pi/4), 0), radius=sphere_size, color=default_color))
+
+g.append(sphere(pos=vector(0, -cos(pi/4), sin(pi/4)), radius=sphere_size, color=default_color))
+g.append(sphere(pos=vector(0, -cos(pi/4), -sin(pi/4)), radius=sphere_size, color=default_color))
+g.append(sphere(pos=vector(sin(pi/4) * cos(pi/4), -cos(pi/4), cos(pi/4) * cos(pi/4)), radius=sphere_size, color=default_color))
+g.append(sphere(pos=vector(sin(pi/4) * cos(pi/4), -cos(pi/4), -cos(pi/4) * cos(pi/4)), radius=sphere_size, color=default_color))
+g.append(sphere(pos=vector(cos(pi/4), -cos(pi/4), 0), radius=sphere_size, color=default_color))
+g.append(sphere(pos=vector(-sin(pi/4) * cos(pi/4), -cos(pi/4), cos(pi/4) * cos(pi/4)), radius=sphere_size, color=default_color))
+g.append(sphere(pos=vector(-sin(pi/4) * cos(pi/4), -cos(pi/4), -cos(pi/4) * cos(pi/4)), radius=sphere_size, color=default_color))
+g.append(sphere(pos=vector(-cos(pi/4), -cos(pi/4), 0), radius=sphere_size, color=default_color))
 
 while 1:
-    rate(60)
+    rate(30)
     for obj in g:
-        g.rotate(angle=radians(1), axis=gaxis, origin=gorigin)
+        obj.rotate(angle=radians(0.5), axis=gaxis, origin=gorigin)
+
+        rand = randint(0, 2)
+        if rand==0:
+            old = obj.red
+            obj.red += uniform(-0.05, 0.05)
+            print("{0} -> {1}".format(old, obj.red))
+        elif rand == 1:
+            old = obj.blue
+            obj.blue += uniform(-0.05, 0.05)
+            print("{0} -> {1}".format(old, obj.blue))
+        else:
+            old = obj.green
+            obj.green += uniform(-0.05, 0.05)
+            print("{0} -> {1}".format(old, obj.green))
