fc694b0984434bb5489f42c6a52b95ba230a5b7d
Robert Jamison <rejamison@gmail.com>
2017-06-19 07:27:22 -0700

    Added test audio file and updated vizualizer test.

--- viz.py
+++ viz.py
@@ -4,7 +4,7 @@ from random import *
 gorigin = vector(0, 0, 0)
 gaxis = vector(0, 1, 0)
 sphere_size = 0.1
-default_color = vector(1.0, 0, 0)
+default_color = vector(0.5, 0.5, 0.5)
 g = []
 g.append(sphere(pos=vector(0, 1, 0), radius=sphere_size, color=default_color))  # top
 
@@ -35,21 +35,40 @@ g.append(sphere(pos=vector(-sin(pi/4) * cos(pi/4), -cos(pi/4), cos(pi/4) * cos(p
 g.append(sphere(pos=vector(-sin(pi/4) * cos(pi/4), -cos(pi/4), -cos(pi/4) * cos(pi/4)), radius=sphere_size, color=default_color))
 g.append(sphere(pos=vector(-cos(pi/4), -cos(pi/4), 0), radius=sphere_size, color=default_color))
 
+# random drift
+#  while 1:
+#     rate(30)
+#     for obj in g:
+#         obj.rotate(angle=radians(0.5), axis=gaxis, origin=gorigin)
+#
+#         oldcolor = obj.color
+#         rand = randint(0, 2)
+#         if rand==0:
+#             obj.color = vector(oldcolor.x + uniform(-0.01, 0.01), oldcolor.y, oldcolor.z)
+#         elif rand == 1:
+#             obj.color = vector(oldcolor.x, oldcolor.y + uniform(-0.01, 0.01), oldcolor.z)
+#         else:
+#             obj.color = vector(oldcolor.x, oldcolor.y, oldcolor.z + uniform(-0.01, 0.01))
+#
+
+# mottle - given two colors, randomly assign each pixel to somewhere between the two
+# and slowly drift each pixel towards one of the colors
+color_a = vector(1, 0, 0)
+color_b = vector(0, 0, 1)
 while 1:
     rate(30)
     for obj in g:
         obj.rotate(angle=radians(0.5), axis=gaxis, origin=gorigin)
 
+        oldcolor = obj.color
         rand = randint(0, 2)
         if rand==0:
-            old = obj.red
-            obj.red += uniform(-0.05, 0.05)
-            print("{0} -> {1}".format(old, obj.red))
+            obj.color = vector(oldcolor.x + uniform(-0.01, 0.01), oldcolor.y, oldcolor.z)
         elif rand == 1:
-            old = obj.blue
-            obj.blue += uniform(-0.05, 0.05)
-            print("{0} -> {1}".format(old, obj.blue))
+            obj.color = vector(oldcolor.x, oldcolor.y + uniform(-0.01, 0.01), oldcolor.z)
         else:
-            old = obj.green
-            obj.green += uniform(-0.05, 0.05)
-            print("{0} -> {1}".format(old, obj.green))
+            obj.color = vector(oldcolor.x, oldcolor.y, oldcolor.z + uniform(-0.01, 0.01))
+
+
+
+
